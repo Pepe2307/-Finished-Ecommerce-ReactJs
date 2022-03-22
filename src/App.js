da435895fe03618591c4a8505f90dc92866d6af4
Pablo Paez <pablopaez2307@gmail.com>
2022-03-22 19:08:45 -0300

    primer update

--- src/App.js
+++ src/App.js
@@ -4,20 +4,50 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+		
+    	<header className="App-nav">
+
+			<img src={logo} className="App-logo" alt="logo" />
+
+			
+
+			<a className="App-link" href="https://reactjs.org" target="_blank"
+			rel="noopener noreferrer">
+			<p>
+			Bienvenido! Seleccione un destino:
+			</p>
+			
+			Where to travel...
+			</a>
+
+      	</header>
+
+
+		<nav className="App-navigation">
+
+			<div className="borde">
+			<img src="../recursos/montana.jpg"></img>
+			Bienvenido a Invento Travels	
+			</div>
+
+			<a className="" href="" target="_blank"
+			rel="noopener noreferrer">
+			<p>
+			Bienvenido! Seleccione un destino
+			</p>
+			
+			Where to travel...
+			</a>
+
+		</nav>
+	
+
+	
+
+    
+
+
+
     </div>
   );
 }
